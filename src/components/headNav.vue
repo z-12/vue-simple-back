73007e93f9f8595fa8c0d9b4f2aee1f93d529f2e
zhaowenjing <web_12@163.com>
2018-09-19 16:42:01 +0800

    header-nav组件

--- src/components/headNav.vue
+++ src/components/headNav.vue
@@ -1,18 +1,12 @@
 <template>
 
-  <div class="header">
-    <el-row>
-      <el-col>
-        <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect" class="header-bar">
-          <el-menu-item index="1">高级插件</el-menu-item>
-          <el-menu-item index="2">在线商城</el-menu-item>
-          <el-menu-item index="3">客户管理</el-menu-item>
-          <el-menu-item index="4">系统设置</el-menu-item>
-          <el-menu-item index="5">活动发布</el-menu-item>
-        </el-menu>
-      </el-col>
-    </el-row>
-  </div>
+  <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect" class="header-bar">
+    <el-menu-item index="1">高级插件</el-menu-item>
+    <el-menu-item index="2">在线商城</el-menu-item>
+    <el-menu-item index="3">客户管理</el-menu-item>
+    <el-menu-item index="4">系统设置</el-menu-item>
+    <el-menu-item index="5">活动发布</el-menu-item>
+  </el-menu>
 
 </template>
 <script>
@@ -30,17 +24,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.header {
-  width: 100%;
-  z-index: 1000;
-  min-width: 1200px;
-  transition: all 0.5s ease;
-  border-top: solid 4px #3091f2;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.12), 0 0 6px 0 rgba(0, 0, 0, 0.04);
-  .header-bar {
-    padding-left: 300px;
-  }
-}
 </style>
 
 
